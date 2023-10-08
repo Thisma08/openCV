baea97a52038ed84638d4903e0674cddf103acfd
Mathis Mahaux <mathis.mahaux@gmail.com>
2023-10-08 14:23:41 +0200

    08/10/23 16:23

--- objectSizes.py
+++ objectSizes.py
@@ -1,4 +1,3 @@
-# import the necessary packages
 from scipy.spatial import distance as dist
 from imutils import perspective
 from imutils import contours
@@ -6,9 +5,9 @@ import numpy as np
 import argparse
 import imutils
 import cv2
+
 def midpoint(ptA, ptB):
 	return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
-# construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
 	help="path to the input image")
