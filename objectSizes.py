f3eb7279db16a9240066d32b07413d24ce46b2ab
Mathis Mahaux <mathis.mahaux@gmail.com>
2023-10-08 14:34:51 +0200

    08/10/23 16:34

--- objectSizes.py
+++ objectSizes.py
@@ -14,3 +14,17 @@ ap.add_argument("-i", "--image", required=True,
 ap.add_argument("-w", "--width", type=float, required=True,
 	help="width of the left-most object in the image (in inches)")
 args = vars(ap.parse_args()
+
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (7, 7), 0)
+
+edged = cv2.Canny(gray, 50, 100)
+edged = cv2.dilate(edged, None, iterations=1)
+edged = cv2.erode(edged, None, iterations=1)
+
+cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+
+(cnts, _) = contours.sort_contours(cnts)
+pixelsPerMetric = None
